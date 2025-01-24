76452faa949f4b48b6a50851cd4b82c8a8e6b747
AvikNayak <nayakavik19@gmail.com>
2025-01-25 00:26:28 +0530

    worked on table component and pagination

--- app/user/properties/page.tsx
+++ app/user/properties/page.tsx
@@ -1,5 +1,53 @@
-const PropertiesPage = () => {
-  return <div>PropertiesPage</div>;
+import prisma from "@/lib/prisma";
+import { getKindeServerSession } from "@kinde-oss/kinde-auth-nextjs/server";
+import PropertiesTable from "./_components/PropertiesTable";
+
+const PAGE_SIZE = 10;
+
+interface Props {
+  searchParams: {
+    [key: string]: string | string[] | undefined;
+  };
+}
+
+const PropertiesPage = async ({ searchParams }: Props) => {
+  const { getUser } = await getKindeServerSession();
+  const user = await getUser();
+
+  const pageNum = searchParams.pagenum ?? 0;
+
+  const propertiesPromise = prisma.property.findMany({
+    where: {
+      userId: user?.id,
+    },
+    include: {
+      type: true,
+      status: true,
+    },
+    skip: +pageNum * PAGE_SIZE,
+    take: PAGE_SIZE,
+  });
+
+  const totalPropertiesPromise = prisma.property.count({
+    where: {
+      userId: user?.id,
+    },
+  });
+
+  const [properties, totalProperties] = await Promise.all([
+    propertiesPromise,
+    totalPropertiesPromise,
+  ]);
+
+  const totalPages = Math.floor(totalProperties / PAGE_SIZE);
+
+  return (
+    <PropertiesTable
+      properties={properties}
+      totalPages={totalPages}
+      currentPage={+pageNum}
+    />
+  );
 };
 
 export default PropertiesPage;
