5e8dfa3d4b67ba56d8f96aae591d5fd6aa621c1a
AvikNayak <nayakavik19@gmail.com>
2025-01-29 00:27:40 +0530

    working on property card

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,34 @@
-export default function Home() {
-  return <div></div>;
+import prisma from "@/lib/prisma";
+import PropertyCard from "./components/PropertyCard";
+import PropertyContainer from "./components/PropertyContainer";
+
+export default async function Home() {
+  const properties = await prisma.property.findMany({
+    select: {
+      id: true,
+      name: true,
+      price: true,
+      images: {
+        select: {
+          url: true,
+        },
+      },
+      location: {
+        select: {
+          city: true,
+          state: true,
+        },
+      },
+    },
+  });
+
+  return (
+    <div>
+      <PropertyContainer>
+        {properties.map((propertyItem) => (
+          <PropertyCard key={propertyItem.id} property={propertyItem} />
+        ))}
+      </PropertyContainer>
+    </div>
+  );
 }
