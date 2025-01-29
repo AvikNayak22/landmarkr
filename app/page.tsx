8bcb9297500e37d38b2ff413a492a80455e3748e
AvikNayak <nayakavik19@gmail.com>
2025-01-29 12:30:25 +0530

    worked on pagination on the home page

--- app/page.tsx
+++ app/page.tsx
@@ -2,8 +2,18 @@ import prisma from "@/lib/prisma";
 import PropertyCard from "./components/PropertyCard";
 import PropertyContainer from "./components/PropertyContainer";
 
-export default async function Home() {
-  const properties = await prisma.property.findMany({
+const PAGE_SIZE = 10;
+
+interface Props {
+  searchParams: {
+    [key: string]: string | string[] | undefined;
+  };
+}
+
+export default async function Home({ searchParams }: Props) {
+  const pagenum = searchParams.pagenum ?? 0;
+
+  const propertiesPromise = prisma.property.findMany({
     select: {
       id: true,
       name: true,
@@ -20,11 +30,22 @@ export default async function Home() {
         },
       },
     },
+    skip: +pagenum * PAGE_SIZE,
+    take: PAGE_SIZE,
   });
+
+  const totalPropertiesPromise = prisma.property.count();
+
+  const [properties, totalProperties] = await Promise.all([
+    propertiesPromise,
+    totalPropertiesPromise,
+  ]);
+
+  const totalPages = Math.floor(totalProperties / PAGE_SIZE);
 
   return (
     <div>
-      <PropertyContainer>
+      <PropertyContainer totalPages={totalPages} currentPage={+pagenum}>
         {properties.map((propertyItem) => (
           <PropertyCard key={propertyItem.id} property={propertyItem} />
         ))}
